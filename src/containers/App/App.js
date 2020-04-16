c1a85f7ec4d67697dfc279eb397cd87f8a4ab709
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-16 15:50:05 -0600

    Add routes and components to render for each location pathname

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
+import Cryptos from '../Cryptos/Cryptos';
+import DetailedCrypto from '../DetailedCrypto/DetailedCrypto';
+import Exchanges from '../Exchanges/Exchanges';
+import FavoriteCardContainer from '../FavoriteCardContainer/FavoriteCardContainer';
 import Header from '../Header/Header';
 import WelcomeCard from '../WelcomeCard/WelcomeCard';
 
@@ -27,6 +31,16 @@ class App extends Component {
         <main className="App">
           <Route exact path='/'>
             <WelcomeCard />
+          </Route>
+          <Route exact path='/cryptos'>
+            <Cryptos />
+          </Route>
+          <Route path='/cryptos/:id' render={(match) => <DetailedCrypto matchId={parseInt(match.match.params.id)} />} />
+          <Route exact path='/exchanges'>
+            <Exchanges />
+          </Route>
+          <Route exact path='/favorites'>
+            <FavoriteCardContainer />
           </Route>
         </main>
       </>
