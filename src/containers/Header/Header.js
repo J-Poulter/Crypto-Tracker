500f8f922f72caf1abcac516b738add603594d1e
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-16 15:32:08 -0600

    Create header's structure and add elements that will be used

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='header-title'>
+        <img className='logo' src='https://image.flaticon.com/icons/svg/2506/2506151.svg' alt='Crypto Tracker logo'/>
+        <h1 className='header-title'>Crypto-Tracker</h1>
+      </div>
+      <nav>
+        <button>Top 100</button>
+        <button>Favorites</button>
+        <button>Home</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
