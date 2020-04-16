a15d2d855ca7ac9737c6a31f61a04b163b79781d
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-16 15:31:32 -0600

    Add structure to the app's render

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+import Header from '../Header/Header';
+import WelcomeCard from '../WelcomeCard/WelcomeCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-   
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      marketInfo: {}
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('https://api.coinlore.net/api/global/')
+    .then(response => response.json())
+    .then(marketInfo => this.setState({marketInfo}))
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <main className="App">
+          <Route exact path='/'>
+            <WelcomeCard />
+          </Route>
+        </main>
+      </>
+    )
+  };
 }
 
 export default App;
