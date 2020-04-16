655fa0cc9e9a0693b939b6ebd5e76deb1b0a3cfc
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-16 17:51:07 -0600

    Display welcome card information

--- src/containers/WelcomeCard/WelcomeCard.js
+++ src/containers/WelcomeCard/WelcomeCard.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './WelcomeCard.css';
 
-const WelcomeCard = () => {
+const WelcomeCard = ({ marketInfo }) => {
+  const { coins_count, active_markets, total_mcap, btc_d, eth_d, avg_change_percent, volume_change } = marketInfo;
+
   return (
-    <div>
-      
-    </div>
+    <section className='welcome-card'>
+      <p>Total Cryptocurrencies: {coins_count}</p>
+      <p>Total Active Exchange Markets: {active_markets}</p>
+      <p>Total Market Cap: ${(total_mcap / 1000000000).toFixed(2)} Billion</p>
+      <p>Bitcoin Market Dominance: {btc_d}%</p>
+      <p>Etherium Market Dominance: {eth_d}%</p>
+      <p>Accumulative Average Value Change (past 24h): {avg_change_percent}%</p>
+      <p>Trading Volume Change (past 24h): {volume_change}%</p>
+      <button className='welcome-button'>Binance Top 100:</button>
+    </section>
   )
 }
 
