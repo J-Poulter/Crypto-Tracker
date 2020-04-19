ca4ff0b5f439402d2661b98a8969b0eee261b9f8
Jeremy Poulter <Jpoulter@gmail.com>
2020-04-19 03:50:43 -0600

    Remove unused imports

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { favorites } from '../../reducers/favorites';
 
 const Header = ({favorites}) => {
   return (
